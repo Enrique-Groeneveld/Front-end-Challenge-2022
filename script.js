f9373a559112e7d023dfe7e7d2c561a3c9730ef6
Enrique Groeneveld <enriquegroeneveld@gmail.com>
2022-02-11 13:32:42 +0100

    Actually save's the results and checks them with the position of the party, also started working on the css

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-var container= document.getElementById("1");
+var container = document.getElementById("container");
 var indicator = 0;
 var specStatement = document.getElementById('specStatement');
 var title = document.getElementById('title');
 var statement = document.getElementById('statement');
 var party = document.getElementById('party');
 var list = document.getElementById('list');
+var result = document.getElementById('result');
+var answers = [];
+container.style.display = 'none' ;
+
+// Sets a match var for every party
+parties.map(p => p.match = 0);
+
+subjects.forEach(subject => {
+	answers.push({
+		title : subject.title,
+		answer : null
+	})
+});
+
+
+
+
+function start(){
+	container.style.display = '' ;
+	document.getElementById('start').style.display = 'none' ;	
+	editText();
+}
 
 function editText (){
 // Sets the title and statement of the subject  
-  title.innerHTML = subjects[indicator]["title"] ;  
-  statement.innerHTML = subjects[indicator]["statement"] ;  
-  unSelect();
-  getParties();
-  
-  indicator += 1;
-}
+	title.innerHTML = subjects[indicator]["title"] ;  
+	statement.innerHTML = subjects[indicator]["statement"] ;  
+	unSelect();
+	getParties();
+
+	indicator += 1;
+
+	if(subjects[indicator] == null){
+		showResult();
+	}
+
+	}
+
 
 function getParties () {
 	let sortedNames = [...subjects[indicator].parties.map(p => p.name)].sort();
@@ -49,13 +77,47 @@ function myFunction() {
 	  x.className = x.className.replace(" w3-show", "");
 	}
   }
+
+
+function answer (choosen){	
+	answers[indicator].answer = choosen;
+	editText();
+}
+
+
+
+function showResult(){
+	result.innerText = 'Resultaat: '	
+	answers.forEach((answer, index )=> {
+		for (party of subjects[index]['parties']){
+			if (answer.answer != "none"){
+				let targetparty = parties.find(p => p.name == party.name);
+				if(answer.answer == party.position){
+					targetparty.match += 1
+				}
+				else {
+					targetparty.match += -1
+				}
+			}
+		}
+	});
+	let count = answers.filter(a => a.answer == "none").length;
+	if (count >= Math.ceil(subjects.length / 2)){
+		console.log("Heeft een mening gebruikt zijn mening niet wat een meme!")
+	}
+	let sorted = parties.sort((a,b) => b.match - a.match);
+
+	for (result2 of sorted){
+		let percentage = (result2.match / subjects.length * 100.).toFixed(0)
+		percentage = percentage < 0 ? 0 : percentage;
+
+		result.innerText += "\n"+ "Percentage: " +  percentage + "Gematched: "+result2.match + " " + result2.name;
+	}
+
+	 
+}
+
   
 
-dparties.forEach((item, index)=>{
-	item.match = 0;
-})
 
-console.log(parties);
-
-  editText();
 
